3e40828c7171579ccd5aa8bd25758336cb8df51f
Mariia Telehonenko <telegonenko1@ukr.net>
2022-09-18 22:39:04 +0300

    Update ImageGalleryItem.js

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { GalleryItem, GalleryImage } from './ImageGalleryItem.styled';
 
-class ImageGalleryItem extends Component {
-  static propTypes = {
-    id: PropTypes.number.isRequired,
-    largeImageURL: PropTypes.string.isRequired,
-    webformatURL: PropTypes.string.isRequired,
-    onShowModal: PropTypes.func.isRequired,
-    tags: PropTypes.string,
-  };
-  render() {
-    const { id, largeImageURL, webformatURL, onShowModal, tags } = this.props;
-
-    return (
-      <GalleryItem key={id}>
-        <GalleryImage
-          src={webformatURL}
-          alt={tags}
-          onClick={() => onShowModal({ largeImageURL, tags })}
-        />
-      </GalleryItem>
-    );
-  }
+function ImageGalleryItem({
+  id,
+  largeImageURL,
+  webformatURL,
+  onShowModal,
+  tags,
+}) {
+  return (
+    <GalleryItem key={id}>
+      <GalleryImage
+        src={webformatURL}
+        alt={tags}
+        onClick={() => onShowModal({ largeImageURL, tags })}
+      />
+    </GalleryItem>
+  );
 }
+
+ImageGalleryItem.propTypes = {
+  id: PropTypes.number.isRequired,
+  largeImageURL: PropTypes.string.isRequired,
+  webformatURL: PropTypes.string.isRequired,
+  onShowModal: PropTypes.func.isRequired,
+  tags: PropTypes.string,
+};
 
 export default ImageGalleryItem;
