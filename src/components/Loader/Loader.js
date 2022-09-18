651111d82f7e67320855df3f435712ff6d780854
Mariia Telehonenko <telegonenko1@ukr.net>
2022-09-18 22:39:14 +0300

    Update Loader.js

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Audio } from 'react-loader-spinner';
-class Loader extends Component {
-  render() {
-    return (
-      <Audio
-        height="100"
-        width="100"
-        color="blue"
-        ariaLabel="loading"
-        wrapperStyle={{
-          position: 'relative',
-          margin: '0 auto',
-        }}
-      />
-    );
-  }
+function Loader() {
+  return (
+    <Audio
+      height="100"
+      width="100"
+      color="blue"
+      ariaLabel="loading"
+      wrapperStyle={{
+        position: 'relative',
+        margin: '0 auto',
+      }}
+    />
+  );
 }
 
 export default Loader;
