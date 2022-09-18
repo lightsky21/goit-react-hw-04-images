a9be71b5d4a6398aea2e3367af03cf2094258605
Mariia Telehonenko <telegonenko1@ukr.net>
2022-09-18 22:39:09 +0300

    Update ImageGalleryItem.styled.js

--- src/components/ImageGalleryItem/ImageGalleryItem.styled.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.styled.js
@@ -12,6 +12,7 @@ export const GalleryImage = styled.img`
   object-fit: cover;
   transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
   :hover {
-  transform: scale(1.03);
-  cursor: zoom-in;
+    transform: scale(1.03);
+    cursor: zoom-in;
+  }
 `;
