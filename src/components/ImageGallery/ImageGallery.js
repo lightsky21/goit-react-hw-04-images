5d4217481301a650f8a1444b08e3ef40714dc967
Mariia Telehonenko <telegonenko1@ukr.net>
2022-09-18 22:39:00 +0300

    Update ImageGallery.js

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,31 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Gallery } from './ImageGallery.styled';
 
 import ImageGalleryItem from 'components/ImageGalleryItem';
 
-class ImageGallery extends Component {
-  static propTypes = {
-    pictures: PropTypes.arrayOf(PropTypes.object.isRequired),
-    onShowModal: PropTypes.func.isRequired,
-  };
-  render() {
-    const { pictures, onShowModal } = this.props;
-    return (
-      <Gallery>
-        {pictures.map(({ webformatURL, largeImageURL, tags }, id) => (
-          <ImageGalleryItem
-            id={id}
-            webformatURL={webformatURL}
-            largeImageURL={largeImageURL}
-            onShowModal={onShowModal}
-            tags={tags}
-          />
-        ))}
-      </Gallery>
-    );
-  }
+function ImageGallery({ pictures, onShowModal }) {
+  return (
+    <Gallery>
+      {pictures.map(({ webformatURL, largeImageURL, tags }, id) => (
+        <ImageGalleryItem
+          id={id}
+          webformatURL={webformatURL}
+          largeImageURL={largeImageURL}
+          onShowModal={onShowModal}
+          tags={tags}
+        />
+      ))}
+    </Gallery>
+  );
 }
+ImageGallery.propTypes = {
+  pictures: PropTypes.arrayOf(PropTypes.object.isRequired),
+  onShowModal: PropTypes.func.isRequired,
+};
 
 export default ImageGallery;
